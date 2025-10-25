bd407cb698cdd999d11b56e7b69ff2d93475d545
ThxBullpup <kinamochi406@gmail.com>
2025-10-25 18:24:03 +0900

    名前が出る様になった

--- assets/js/quizgame.js
+++ assets/js/quizgame.js
@@ -1,21 +1,28 @@
 let allCharacters = [];
 let currentCharacters = null;
 
+//htmlの読み込み時に生徒名簿を読み込む関数
 async function loadStudent(){
     const response = await fetch("./student-roster.json");
     allCharacters = await response.json();
-    console.log(allCharacters.length,allCharacters[1]);
 }
 
+//出題される生徒を選ぶ
 function chooseStudent(){
     choice = Math.floor(Math.random() * allCharacters.length);
+    //選ばれた生徒のIDと名前から使用する画像の名前を生成する
     faceImageAddress = `./assets/images/${('0000' + allCharacters[choice].id).slice( -4 ) + "-" + allCharacters[choice].firstname.english}.webp`;
-    console.log(faceImageAddress);
+    //画像と名前を置き換え
+    document.querySelector(".faceAndName").hidden = false;
     document.querySelector("#faceImage").setAttribute("src", faceImageAddress);
+    document.querySelector("#firstName").textContent = allCharacters[choice].firstname.kana;
 }
+
+
 //ロード完了時に生徒名簿を読み込む
 document.addEventListener("DOMContentLoaded", async() => {
     await loadStudent();
 })
 
+//ボタンが押されたら生徒を選ぶ関数へ
 document.querySelector("#start").addEventListener("click",chooseStudent);
