fbfc00e1aba762017babd71b23148c4d684cb749
ThxBullpup <kinamochi406@gmail.com>
2025-11-01 14:53:38 +0900

    色々最適化

--- assets/js/quizgame.js
+++ assets/js/quizgame.js
@@ -34,7 +34,7 @@ async function filterGenerate(){
     ]
     // チェックボックスにして表示、改行
     const container = document.getElementById('school');
-    i = 0;
+    let i = 0;
     schools.forEach(schoolName => {
         const filterCheckbox =`
             <label>
@@ -50,51 +50,53 @@ async function filterGenerate(){
     })
 }
 
-// 出題リストの初期化
-function initializeQuizList(){
-
-}
-
 //出題される生徒を選ぶ
 function chooseStudent(){
     const candidateStudents = MXQuiz.allStudents;
-    //出題数ぶん生徒を選んで、そのIDをchoiceStudentsに格納
+    //出題数ぶん生徒を選んでchoiceStudentsに格納
     for(let i = 0; i < MXQuiz.totalQuestions; i+=1){
-        MXQuiz.choiceStudents.push(candidateStudents[Math.floor(Math.random() * candidateStudents.length)].id);
+        MXQuiz.choiceStudents.push(candidateStudents[Math.floor(Math.random() * candidateStudents.length)]);
     }
 }
 
     //解答を判定する
 function checkAnswer(){
-    document.getElementById('userAnswer')
-    const correctAnswer = MXQuiz.allStudents[MXQuiz.choiceStudents[MXQuiz.currentQuestion]].firstname.kana
-    if (userAnswer.value == correctAnswer){
+    const userAnswerElement = document.getElementById('userAnswer');
+    const correctAnswer = MXQuiz.choiceStudents[MXQuiz.currentQuestion].firstname.kana;
+
+    // 得点
+    if (userAnswerElement.value == correctAnswer){
         MXQuiz.score += 1;
         console.log('正解!!シャーレに+1点')
     }else{
         console.log('残念!!しっかりしな')
     }
-    userAnswer.value =''
+    // テキストボックスをクリア
+    userAnswerElement.value =''
+    // 問題数を進める
     MXQuiz.currentQuestion += 1;
+    // 現在の問題が設定した問題数以下なら継続、規定数を超えたら結果画面へ
     if (MXQuiz.currentQuestion < MXQuiz.totalQuestions){
         displayQuestion();
     }else{
         console.log('result');
         transitionResult();
     }
-
 }
 
+// クイズ画面の表示
 function displayQuestion(){
     // 今何問目か表示
     const container = document.getElementById('questionCounter');
-    const questionCount = `第${MXQuiz.currentQuestion+1}問`
+    const questionCount = `第${MXQuiz.currentQuestion+1}問`;
         container.innerHTML = questionCount; //ループの問題が治ったらインクリメントを代入に変更
-    //選ばれた生徒のIDと名前から使用する画像の名前を生成する
-    faceImageAddress = `./assets/images/${('0000' + MXQuiz.choiceStudents[MXQuiz.currentQuestion]).slice( -4 ) + '-' + MXQuiz.allStudents[MXQuiz.choiceStudents[MXQuiz.currentQuestion]].firstname.english}.webp`;//全生徒[選ばれた生徒のID]下の名前の英語
-    //画像と名前を置き換え
-    document.querySelector('#answerSend').addEventListener('click',{name: MXQuiz.allStudents[MXQuiz.choiceStudents[MXQuiz.currentQuestion]].firstname.kana}); document.querySelector('#faceImage').setAttribute('src', faceImageAddress);
-    document.querySelector('#firstName').textContent = MXQuiz.allStudents[MXQuiz.choiceStudents[MXQuiz.currentQuestion]].firstname.kana;
+    // 選ばれた生徒のIDと名前から使用する画像の名前を生成する
+    const faceImageAddress = `./assets/images/${('0000' + MXQuiz.choiceStudents[MXQuiz.currentQuestion].id).slice( -4 ) + '-' + MXQuiz.choiceStudents[MXQuiz.currentQuestion].firstname.english}.webp`;//全生徒[選ばれた生徒のID]下の名前の英語
+    // 画像と名前を置き換え
+    const firstNameElement = document.getElementById('firstName');
+    const faceImageElement = document.getElementById('faceImage');
+    firstNameElement.textContent = MXQuiz.choiceStudents[MXQuiz.currentQuestion].firstname.kana
+    faceImageElement.src = faceImageAddress;
 }
 
 //クイズの画面に遷移する
@@ -105,8 +107,6 @@ function transitionQuiz(){
     // 出題する生徒を選んでchoiceStudentsに格納
     chooseStudent();
     displayQuestion();
-    document.querySelector('#answerSend').addEventListener('click',checkAnswer);
-
 }
 
 // タイトル画面に戻る
@@ -126,15 +126,21 @@ function transitionResult(){
     document.getElementById('quiz').style.display = 'none';
     document.getElementById('result').style.display = 'block';
     // はじめの画面に戻るボタン
-    document.querySelector('#repeat').addEventListener('click',transitionTitle());
 }
 
-
-//ロード完了時に生徒名簿を読み込み、出題フィルターを生成
-document.addEventListener('DOMContentLoaded', async() => {
+// 初期化する処理（一回だけ実行する）
+function initQuiz(){
+    //ロード完了時に生徒名簿を読み込み、出題フィルターを生成
+    document.addEventListener('DOMContentLoaded', async() => {
     await loadStudent();
     await filterGenerate();
-})
+    })
+    //ボタンが押されたら生徒を選ぶ関数へ移動
+    document.querySelector('#start').addEventListener('click',transitionQuiz);
+    // もう一度のボタンを押すとタイトルへ
+    document.querySelector('#repeat').addEventListener('click',transitionTitle);
+    //解答ボタンを押すと正誤判定関数へ移動
+    document.querySelector('#answerSend').addEventListener('click',checkAnswer);
+}
 
-//ボタンが押されたら生徒を選ぶ関数へ
-document.querySelector('#start').addEventListener('click',transitionQuiz);
+initQuiz();
